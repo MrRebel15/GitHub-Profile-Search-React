566753495fd40ad955a17ce23c38aac1485a9ff9
Bhavesh <bhaveshmandavkar40@gmail.com>
2021-01-27 23:38:06 +0530

    first commit

---
+++ src/Config/Firebaseconfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCnZUe7HC2_6ADcSf5yYqe0tgq1bEhOP_k",
+    authDomain: "github-app-9644b.firebaseapp.com",
+    projectId: "github-app-9644b",
+    storageBucket: "github-app-9644b.appspot.com",
+    messagingSenderId: "584880142005",
+    appId: "1:584880142005:web:ac3fdab0f1ac2174d8b0fc",
+    measurementId: "G-NM1JY2VBSE"
+  };
---
+++ src/Pages/PageNotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>404 Page Not Found Page</h1>
+    </div>
+  );
+};
+export default PageNotFound;
